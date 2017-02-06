951680dcdf7750822a24b855f3a197c6afee8337
kentah <kenthow@gmail.com>
2017-02-06 12:41:31 -0600

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/Header';
+import LinkCreate from './components/LinkCreate';
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <LinkCreate />
+    </div>
+  );
+};
+
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.render-target'));
+})
