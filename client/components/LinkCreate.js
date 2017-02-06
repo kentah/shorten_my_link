71853038c753a3dd2794233084aaded3adb603d1
kentah <kenthow@gmail.com>
2017-02-06 13:40:46 -0600

    Add url validation via Match and check from meteor/check

--- client/components/LinkCreate.js
+++ client/components/LinkCreate.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 
 
 class LinkCreate extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    Meteor.call('links.insert', this.refs.link.value);
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className='form-group'>
           <label>Link to shorten</label>
-          <input className='form-control' />
+          <input ref='link' className='form-control' />
         </div>
         <button className='btn btn-primary'>Shorten!</button>
       </form>
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,4 +1,12 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
+
+Meteor.methods({
+  'links.insert': url => {
+    check(url, Match.Where(url => validUrl.isUri(url)));
+  }
+});
 
 export const Links = new Mongo.Collection('links');
